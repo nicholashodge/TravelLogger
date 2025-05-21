1ed65782275637dec7e9875f3c1d9f06bc839e8e
Joey Blackwell <blackwellj6241@gmail.com>
2025-05-21 10:35:38 -0500

    fixed city by id endpoint

--- Models/City.cs
+++ Models/City.cs
@@ -5,6 +5,7 @@ public class City
 {
     public int Id { get; set; }
     [Required]
-    public string Name { get; set; } 
+    public string Name { get; set; }
     public List<Log>? Logs { get; set; }
+    public Recommendation? Recommendation { get; set; }
 }
